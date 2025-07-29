a79ef2854ce632e47a170c6499638817ca0451c7
MaheshPatil31 <maheshpateel31@gmail.com>
2025-07-29 09:48:16 +0530

    Create 027-Remove_Element.cpp

---
+++ 027-Remove_Element.cpp
@@ -0,0 +1,15 @@
+// LeetCode 027. Remove Element
+// https://leetcode.com/problems/remove-element/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        for(int i=0; i<nums.size(); i++){
+            if(val==nums[i]){
+                nums.erase(nums.begin()+i);
+                i--;
+            }
+        }
+        return nums.size();
+    }
+};
