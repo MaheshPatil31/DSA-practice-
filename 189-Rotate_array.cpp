44a388801400cca991e5deb86bbdb10a40da9579
MaheshPatil31 <maheshpateel31@gmail.com>
2025-07-29 09:59:00 +0530

    Create 189-Rotate_array.cpp

---
+++ 189-Rotate_array.cpp
@@ -0,0 +1,18 @@
+// LeetCode 189. Rotate Array
+// https://leetcode.com/problems/rotate-array/
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+    
+        int n = nums.size();
+        k = k % n; 
+        
+        vector<int> temp(n);
+        
+        for(int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+        
+        nums = temp;
+    }
+};
