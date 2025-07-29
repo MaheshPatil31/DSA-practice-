7251dbe91d3e86d30985a57a5996dd2716bcbe52
MaheshPatil31 <maheshpateel31@gmail.com>
2025-07-29 10:31:50 +0530

    Create 001-Two_Sum.cpp

---
+++ 001-Two_Sum.cpp
@@ -0,0 +1,15 @@
+// leetcode 1. Two sum
+// https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
+        for(int i=0; i<nums.size(); i++) map[nums[i]] = i;
+        for(int i=0; i<nums.size(); i++){
+            int comp=target-nums[i];
+            if(map.count(comp) && map[comp]!= i) return {i, map[comp]};
+        }
+        return {};
+    }
+};
